7f86eed505d7794299059d9fd7fcbfc57e890e8b
MartinezLopezYA <martinezlopezyersonandres.ing@gmail.com>
2023-08-23 22:38:19 -0500

    Initial commit

---
+++ src/main/java/com/example/trabajodegrado/TrabajodegradoApplication.java
@@ -0,0 +1,13 @@
+package com.example.trabajodegrado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajodegradoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajodegradoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/trabajodegrado/TrabajodegradoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trabajodegrado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajodegradoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
