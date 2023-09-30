82f4dfeeaa122996f4a45f9917277ae8a1f763bc
DEVANDRES <martinezlopezyersonandres.ing@gmail.com>
2023-09-29 21:34:05 -0500

    Pantalla perfil, ajuste general del proyecto

--- front/trabajo_de_grado/src/app/app-routing.module.ts
+++ front/trabajo_de_grado/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './containers/login/login.component';
 import { SinginComponent } from './containers/singin/singin.component';
+import { PerfilComponent } from './containers/acciones/perfil/perfil.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent},
   { path: 'singin', component: SinginComponent},
+  { path: 'perfil', component: PerfilComponent},
 
   { path: '', redirectTo: '/login', pathMatch: 'full'},
 
--- front/trabajo_de_grado/src/app/app.module.ts
+++ front/trabajo_de_grado/src/app/app.module.ts
@@ -5,7 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './containers/login/login.component';
 import { SinginComponent } from './containers/singin/singin.component';
-import { PerfilComponent } from './containers/perfil/perfil.component';
+import { PerfilComponent } from './containers/acciones/perfil/perfil.component';
+import { InicioComponent } from './containers/acciones/inicio/inicio.component';
+import { RecursosComponent } from './containers/acciones/recursos/recursos.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormPerfilComponent } from './containers/acciones/perfil/modal/form-perfil/form-perfil.component';
 
 
 @NgModule({
@@ -14,11 +21,20 @@ import { PerfilComponent } from './containers/perfil/perfil.component';
     LoginComponent,
     SinginComponent,
     PerfilComponent,
+    InicioComponent,
+    RecursosComponent,
+
+    FormPerfilComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     
+    
+    //Angular Material
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
